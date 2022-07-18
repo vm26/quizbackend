f1ac2c3edbdaf97dd758a766ad799c0a33942498
vm26 <91827717+vm26@users.noreply.github.com>
2022-07-18 14:20:45 +0530

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 //CORS
 app.use(cors());
 app.use('/',router);
-var PORT= 5000;
+var PORT= process.env.PORT || 5000;
 
 mongoose.connect('mongodb+srv://varnisha26:varnisha26@cluster0.lca0p.mongodb.net/quizapp').then(()=>{
     app.listen(PORT,()=>console.log(`Server is listening to ${PORT}`) )
